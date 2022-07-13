ddee05f1c7003e7323aad84c8f2926b4da17ced2
M <bhargavi.c.m@capgemini.com>
2022-07-13 18:23:20 +0530

    added canteen-app

---
+++ canteen-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './components/counter';
+import Test from './components/test';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Test />
+    </div>
+  );
+}
+
+export default App;
